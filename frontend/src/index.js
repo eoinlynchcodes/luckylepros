68af268c63af224b399ea2e2c3ff272ad2a808f6
eoinlynchcodes <eoin95lynch@gmail.com>
2021-12-04 14:20:58 +0100

    first deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
-  <ChakraProvider>
+  <React.StrictMode>
     <App />
-  </ChakraProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
