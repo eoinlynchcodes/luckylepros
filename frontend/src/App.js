4317c74866da7d2b479af4ae1016717ece3c5702
eoinlynchcodes <eoin95lynch@gmail.com>
2021-12-06 15:33:30 +0100

    deploy on correct domain

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Lucky Lepros</h1>
       </div>
   );
